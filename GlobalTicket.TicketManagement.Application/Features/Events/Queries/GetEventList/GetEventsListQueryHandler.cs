5700257f53c21f4d71d9801494ddf8d6f5c75d88
Olufikunmi Ogunye <ogunye_kunmi@yahoo.com>
2024-08-27 18:07:14 +0100

    Application Layer: Event Request Handler

---
+++ GlobalTicket.TicketManagement.Application/Features/Events/Queries/GetEventList/GetEventsListQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using GlobalTicket.TicketManagement.Application.Contracts;
+using GlobalTicket.TicketManagement.Domain.Entites;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Application.Features.Events.Queries.GetEventList
+{
+    public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListVm>>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventsListQueryHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+        public async Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.GetAllAsync()).OrderBy(x => x.EventDate);
+            return _mapper.Map<List<EventListVm>>(allEvents);
+        }
+    }
+}
