34a73700231260db3a3c481dd6ef7539324d74f3
Olufikunmi Ogunye <ogunye_kunmi@yahoo.com>
2024-09-09 16:37:45 +0100

    Add CQRS - Command request using Mediator R

---
+++ GlobalTicket.TicketManagement.Application/Features/Events/Commands/Delete Event/DeleteEventCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using GlobalTicket.TicketManagement.Application.Contracts.Persistence;
+using GlobalTicket.TicketManagement.Application.Exceptions;
+using GlobalTicket.TicketManagement.Domain.Entites;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Application.Features.Events.Commands.Delete_Event
+{
+    public class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteEventCommandHandler(IAsyncRepository<Event> eventRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+        {
+            var eventToDelete = await _eventRepository.GetByIdAsync(request.EventId);
+
+            if(eventToDelete == null)
+            {
+                throw new NotFoundException(nameof(Event), request.EventId);
+            }
+
+            await _eventRepository.DeleteAsync(eventToDelete);
+
+            return Unit.Value;
+        }
+    }
+}
