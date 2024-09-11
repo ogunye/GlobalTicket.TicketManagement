884f7fd48a96e06f39a09aa5b7dff227c490f83e
Olufikunmi Ogunye <ogunye_kunmi@yahoo.com>
2024-09-11 06:56:23 +0100

    CQRS- Command Request for Category and Order

---
+++ GlobalTicket.TicketManagement.Application/Features/Orders/GetOrderForMonth/GetOrdersForMonthQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using GlobalTicket.TicketManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Application.Features.Orders.GetOrderForMonth
+{
+    public class GetOrdersForMonthQueryHandler : IRequestHandler<GetOrdersForMonthQuery, PagedOrdersForMonthVm>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersForMonthQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<PagedOrdersForMonthVm> Handle(GetOrdersForMonthQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _orderRepository.GetPagedOrdersForMonth(request.Date, request.Page, request.Size);
+            var orders = _mapper.Map<List<OrdersForMonthDto>>(list);
+
+            var count = await _orderRepository.GetTotalCountOfOrdersForMonth(request.Date);
+
+            return new PagedOrdersForMonthVm() { Count = count, OrderForMonth = orders, Page = request.Page, Size = request.Size };
+            
+        }
+    }
+}
---
+++ GlobalTicket.TicketManagement.Application/Features/Orders/GetOrderForMonth/PagedOrdersForMonthVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Application.Features.Orders.GetOrderForMonth
+{
+    public class PagedOrdersForMonthVm
+    {
+        public int Count { get; set; }
+        public int Page { get; set; }
+        public int Size { get; set; }
+
+        public ICollection<OrdersForMonthDto>? OrderForMonth { get; set; }
+    }
+}
